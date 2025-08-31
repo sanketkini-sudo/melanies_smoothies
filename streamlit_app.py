898b563b3d849d4535d1fa0f5c0beb190c504659
sanketkini-sudo <sanket.kini@gmail.com>
2025-09-01 02:36:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,4 +52,5 @@ if ingredient_list:
 #New section to request smoothiefroot nutrition information 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
